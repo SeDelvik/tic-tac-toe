3332d2abf6cd1d462933d1263c77b065297cc2f8
adubrovina <adubrovina@at-consulting.ru>
2023-03-28 13:30:40 +0300

    init

---
+++ app/src/main/java/com/example/myapplication/MainFragment.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.myapplication.databinding.FragmentMainBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_main, container, false)
+        val binding = DataBindingUtil.inflate<FragmentMainBinding>(inflater, R.layout.fragment_main,container,false)
+        binding.button.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_mainFragment_to_gameFragment)
+        }
+        return binding.root
+    }
+
+}
